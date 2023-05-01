c96b9baa897a736703abf86318a4c8341d4ca886
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-05-01 13:30:26 +0700

    add IcategoriesService interface

---
+++ Do-An/Data/Services/ICategoriesService.cs
@@ -0,0 +1,14 @@
+﻿using Do_An.Models;
+using System.Collections.Generic;
+
+namespace Do_An.Data.Services
+{
+    public interface ICategoriesService
+    {
+        IEnumerable<Category> GetAll();
+        Category GetById(int id);
+        void Add(Category category);
+        Category Update(int id, Category newCategory);
+        void Delete(int id);
+    }
+}
