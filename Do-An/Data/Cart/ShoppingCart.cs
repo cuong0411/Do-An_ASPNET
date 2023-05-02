c8e12fac59e9399fa68f1164aea513ab1bf3e3d8
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-05-02 14:34:35 +0700

    AdAddItemToCart and RemoveItemFromCart

--- Do-An/Data/Cart/ShoppingCart.cs
+++ Do-An/Data/Cart/ShoppingCart.cs
@@ -1,6 +1,7 @@
 ﻿using Do_An.Migrations;
 using Do_An.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -15,6 +16,47 @@ namespace Do_An.Data.Cart
         public ShoppingCart(AppDbContext appDbContext)
         {
             this.appDbContext = appDbContext;
+        }
+        public void AddItemToCart(Product product)
+        {
+            var shoppingCartItem = appDbContext.ShoppingCartItems
+                .FirstOrDefault(n => n.Product.Id == product.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                // create new shoppingCartItem
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Product = product,
+                    Amount = 1
+                };
+
+                appDbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            appDbContext.SaveChanges();
+        }
+        public void RemoveItemFromCart(Product product)
+        {
+            var shoppingCartItem = appDbContext.ShoppingCartItems
+                .FirstOrDefault(n => n.Product.Id == product.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                }
+                else
+                {
+                    appDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+            appDbContext.SaveChanges();
         }
 
         public List<ShoppingCartItem> GetShoppingCartItems()
