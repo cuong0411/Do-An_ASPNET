bf5daa3de31395e3164b0e91a533498a034b3901
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-05-02 14:05:17 +0700

    setting up shoppingcart class

---
+++ Do-An/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using Do_An.Migrations;
+using Do_An.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Do_An.Data.Cart
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext appDbContext;
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = appDbContext.ShoppingCartItems
+                .Where(s => s.ShoppingCartId == ShoppingCartId)
+                .Include(s => s.Product).ToList());
+        }
+        public double GetShoppingCartTotal()
+        {
+            return appDbContext.ShoppingCartItems
+                .Where(s => s.ShoppingCartId == ShoppingCartId)
+                .Select(n => n.Product.Price * n.Amount).Sum();
+        }
+    }
+}
