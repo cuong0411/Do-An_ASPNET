d60c7434e9e3c9878da8d6993c73d6d960e55f1c
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-05-02 22:18:28 +0700

    add Identity nuget and migration to db

--- Do-An/Data/AppDbContext.cs
+++ Do-An/Data/AppDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Do_An.Models;
+using Do_An.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Do_An.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
---
+++ Do-An/Models/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Do_An.Models.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "Họ Tên")]
+        public string FullName { get; set; }
+    }
+}
