e0edd8301261ab187d61d98d5fd2373083b0a07b
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-05-02 20:25:26 +0700

    ViewComponent

---
+++ Do-An/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,21 @@
+﻿using Do_An.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Do_An.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            this.shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = shoppingCart.GetShoppingCartItems();
+            return View(items.Count);
+        }
+    }
+}
