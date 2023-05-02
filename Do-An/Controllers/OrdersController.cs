f1742958195775ecbb1960e4bfecb96404c3c7d8
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-05-02 14:42:08 +0700

    add orders controller

---
+++ Do-An/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using Do_An.Data.Cart;
+using Do_An.Data.Services;
+using Do_An.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Do_An.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IProductsService productsService;
+        private readonly ShoppingCart shoppingCart;
+
+        public OrdersController(IProductsService productsService, ShoppingCart shoppingCart)
+        {
+            this.productsService = productsService;
+            this.shoppingCart = shoppingCart;
+        }
+        public IActionResult Index()
+        {
+            var items = shoppingCart.GetShoppingCartItems();
+            shoppingCart.ShoppingCartItems = items;
+            var response = new ShoppingCartDTO()
+            {
+                ShoppingCart = shoppingCart,
+                ShoppingCartToal = shoppingCart.GetShoppingCartTotal()
+            };
+            return View(response);
+        }
+    }
+}
---
+++ Do-An/Models/DTO/ShoppingCartDTO.cs
@@ -0,0 +1,10 @@
+﻿using Do_An.Data.Cart;
+
+namespace Do_An.Models.DTO
+{
+    public class ShoppingCartDTO
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartToal { get; set; }
+    }
+}
