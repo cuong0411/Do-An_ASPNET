81f9e7e3a37a11397651afb10a504b7c45236ffa
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-05-03 12:39:29 +0700

    add model Login, AccountController and design login form

---
+++ Do-An/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Do_An.Data;
+using Do_An.Models.DTO;
+using Do_An.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Do_An.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly AppDbContext appDbContext;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext appDbContext)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.appDbContext = appDbContext;
+        }
+        public IActionResult Login() => View(new Login());
+    }
+}
---
+++ Do-An/Models/DTO/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Do_An.Models.DTO
+{
+    public class Login
+    {
+        [Display(Name = "Địa chỉ email")]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
