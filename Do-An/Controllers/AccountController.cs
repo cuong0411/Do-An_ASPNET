5da16de6cc1e2364841d93bcaa35f92e5b61db29
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-05-03 14:21:48 +0700

    output error when signup fail

--- Do-An/Controllers/AccountController.cs
+++ Do-An/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using Do_An.Data;
+using Do_An.Data.Static;
 using Do_An.Models.DTO;
 using Do_An.Models.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Do_An.Controllers
@@ -47,5 +49,45 @@ namespace Do_An.Controllers
         }
 
         public IActionResult Register() => View(new Register());
+        [HttpPost]
+        public async Task<IActionResult> Register(Register register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(register);
+            }
+
+            var user = await userManager.FindByEmailAsync(register.Email);
+            if (user != null)
+            {
+                TempData["Error"] = "This email is already in use";
+                return View(register);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = register.FullName,
+                Email = register.Email,
+                UserName = register.Email
+            };
+            var newUserReponse = await userManager.CreateAsync(newUser, register.Password);
+            if (newUserReponse.Succeeded)
+            {
+                await userManager.AddToRoleAsync(newUser, UserRoles.User);
+                return View("RegisterCompleted");
+            }
+            else
+            {
+                var errors = newUserReponse.Errors;
+                StringBuilder sb = new StringBuilder();
+                foreach (var error in errors)
+                {
+                    sb.Append(error.Description);
+                    sb.AppendLine();
+                }
+                TempData["Error"] = sb.ToString();
+                return View(register);
+            }
+        }
     }
 }
